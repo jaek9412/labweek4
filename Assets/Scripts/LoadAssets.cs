bbf02e6e8a41413ce48ff42201d4cb0a70b32c3c
Young Hyun Kim <69833859+jaek9412@users.noreply.github.com>
2020-08-18 14:51:49 +1000

    70 percent complete

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class LoadAssets : MonoBehaviour
 {
     public GameObject redObj;
+    [SerializeField] private GameObject blueObj;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Spawn in redObj at (2, 0, 0)
+        Instantiate(redObj, new Vector3(2, 0, 0), Quaternion.identity);
+
+        // Spawn in blueObj at (-2, 0, 0)
+        Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
