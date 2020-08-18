11b4924d2b2d17e9c185c7932ea4aaf6ab78df1e
Young Hyun Kim <69833859+jaek9412@users.noreply.github.com>
2020-08-18 15:29:57 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class PrintAndHide : MonoBehaviour
 {
     public Renderer rend;
-
     private int count;
+    private int rand;
     // Start is called before the first frame update
     void Start()
     {
         count = 0;
+        rand = Random.Range(200, 250);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (gameObject.CompareTag("Red") && count == 100)
+        {
+            gameObject.SetActive(false);
+        }
+
+        if (gameObject.CompareTag("Blue") && count == rand)
+        {
+            rend.enabled = false;
+        }
         Debug.Log(gameObject.name + ": " + count);
         count++;
     }
